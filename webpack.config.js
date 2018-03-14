46ad1884438945b02abb0d4044dcf89552d97fbf
Greentea1120 <741024920@qq.com>
2018-03-15 00:52:22 +0800

    配置webpack

---
+++ webpack.config.js
@@ -0,0 +1,81 @@
+const path = require('path');
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist/',
+    filename: 'js/app.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /(node_modules)/,  //对这个不做处理
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['env','react']
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'resource/[name].[ext]'   // 路径
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+              name: 'resource/[name].[ext]'   // 路径
+            }
+          }
+        ]
+      },
+      
+    ]
+  },
+  plugins: [
+    // 处理HTML文件
+    new HtmlWebpackPlugin({
+        template: './src/index.html'
+    }),
+    // 独立css文件
+    new ExtractTextPlugin("css/[name].css"),
+    // 提出公共模块
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common',   // 公共模块名
+      filename: 'js/base.js'  // 打包的目录
+    })
+  ],
+  devServer: {
+    port: 8086
+  }
+};
